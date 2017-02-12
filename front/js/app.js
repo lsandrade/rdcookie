11845ab9ecd176b386e1936f8bf51b7afff8374e
Luan Andrade <luansandrade92@hotmail.com>
2017-02-12 15:36:57 -0200

    verify cookies

---
+++ front/js/app.js
@@ -0,0 +1,25 @@
+
+//Generate Uid
+//source: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+
+
+//cookies
+function verifyCookies(){
+  if(!Cookies.get('name')){
+    Cookies.set('name',guid());
+    console.log('nao ha cookies');
+  }
+  else{
+    console.log(Cookies.get('name'));
+  }
+}
