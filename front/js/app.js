fa85b1ca9141d4696f6dac4ddcff2003364f67e7
Luan Andrade <luansandrade92@hotmail.com>
2017-02-12 18:35:00 -0200

    send log to api

--- front/js/app.js
+++ front/js/app.js
@@ -1,7 +1,7 @@
-
+var App = function() {
 //Generate Uid
 //source: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function guid() {
+var guid = function () {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
@@ -9,12 +9,12 @@ function guid() {
   }
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
     s4() + '-' + s4() + s4() + s4();
-}
+};
 
 
 
 //cookies
-function verifyCookies(){
+var verifyCookies = function(){
   if(!Cookies.get('name')){
     Cookies.set('name',guid());
     console.log('nao ha cookies');
@@ -22,24 +22,38 @@ function verifyCookies(){
   else{
     console.log(Cookies.get('name'));
   }
-}
+};
 
-function sendCookies(address){
-  var person = { cookie: Cookies.get('name'),
-                  page: address };
+
+
+var sendCookies = function(address){
+  var person = { log:{
+                  cookie: Cookies.get('name'),
+                  page: address
+                  }
+                };
   $.ajax({
-            url: 'http://127.0.0.1:3000/logs.json',
-            type: 'POST',
-            dataType: 'json',
-            success: function (data) {
-                console.log(data);
-            },
-            data: person
-        });
-
+        url: 'http://127.0.0.1:3000/logs.json',
+        type: 'POST',
+        dataType: 'json',
+        success: function (data) {
+              console.log(data);
+        },
+        data: person
+  });
 }
 
-function sendEmail(){
+var sendEmail = function(){
   console.log(document.getElementById('email').value);
   console.log('a')
 }
+
+
+return {
+  init: function(address){
+    verifyCookies();
+    sendCookies(address);
+  }
+}
+
+}();
